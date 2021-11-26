4b7beb0b987217aacb373ef3544d1be7ebef612e
Zeronos <boss16805@gmail.com>
2021-11-26 16:43:19 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,19 @@
 #include<iostream>
 using namespace std;
 
-int main(){
+int main(){ 
+    int N,e = 0 ,o = 0;
+    while(N != 0){
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> N;
+    if(N%2 == 0){
+        e++;
+    }
+    if(N%2 != 0){
+        o++;
+    }
+    }
+    cout << "#Even numbers = " << e << "\n";
+    cout << "#Odd numbers = " << o << "\n";
     return 0;
 }
